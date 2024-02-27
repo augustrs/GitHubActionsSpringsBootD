38dfbca19388bef249d9b041fed66dfb77da7606
augustrs <auso0001@stud.kea.dk>
2024-02-27 13:27:23 +0100

    Initial commit

---
+++ src/main/java/com/example/githubactionsspringsbootd/GitHubActionsSpringsBootDApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsspringsbootd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsSpringsBootDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionsSpringsBootDApplication.class, args);
+    }
+
+}
